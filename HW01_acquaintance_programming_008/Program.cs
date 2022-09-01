bdcb35cdc97aea1740df19756b43eef118cbdc66
Konstantin Kochetkov <kmkochetkov@gmail.com>
2022-09-01 20:48:47 +0300

    Задача 8 ДЗ 001 C#

---
+++ HW01_acquaintance_programming_008/Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число N:");
+int number = int.Parse(Console.ReadLine());
+int numberT = 2;
+//Console.Write("Введите число для проверки на чётность: ");
+//int number = int.Parse(Console.ReadLine());
+
+Console.Write($"Чётные числа в диапазоне [1 - {number}] : ");
+
+while (numberT < number)
+{
+    Console.Write($" {numberT}");
+    numberT = (numberT+2);
+}
+Console.WriteLine();
