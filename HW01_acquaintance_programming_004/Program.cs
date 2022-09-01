6a57ebd7e9fba9ad8d1a438cdb69c3ab683e7f83
Konstantin Kochetkov <kmkochetkov@gmail.com>
2022-09-01 20:47:59 +0300

    Задача 4 ДЗ 001 C#

---
+++ HW01_acquaintance_programming_004/Program.cs
@@ -0,0 +1,28 @@
+﻿//Домашнее задание 1 (Знакомство с языками программирования)
+// Задача 4
+// программа, принимает на вход три числа и выдаёт
+// максимальное из этих чисел.
+
+Console.WriteLine("Введите первое число");
+int numberFirst = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int numberSecond = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int numberThird = int.Parse(Console.ReadLine());
+
+int numberMax = numberFirst;
+
+if (numberSecond > numberMax)
+{
+    numberMax = numberSecond;
+}
+
+if (numberThird > numberMax)
+{
+    numberMax = numberThird;
+}
+
+Console.WriteLine($"Максимальное число: {numberMax}");
+
